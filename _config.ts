d3e83c6b9cd9467a26590a1ed19e7dd8ccd3a73a
Filip Seman <filip.seman@protonmail.com>
2023-08-13 08:41:40 +0200

    feat: migrate to lume

---
+++ _config.ts
@@ -0,0 +1,30 @@
+import lume from "lume/mod.ts";
+
+import esbuild from "lume/plugins/esbuild.ts";
+import sitemap from "lume/plugins/sitemap.ts";
+import inline from "lume/plugins/inline.ts";
+import minifyHTML from "lume/plugins/minify_html.ts";
+import lightningCss from "lume/plugins/lightningcss.ts";
+
+const site = lume({
+	/** required for sitemap() */
+	location: new URL("http://vsdent.sk"),
+});
+
+site
+	.copy("css")
+	.copy("img")
+	.copy("svg")
+	.use(sitemap())
+	.use(inline())
+	.use(minifyHTML())
+	.use(esbuild({
+		extensions: [".js", ".ts"],
+		options: { treeShaking: true, minify: true },
+	}))
+	/** automaticly copy static files based on css import */
+	.use(lightningCss({
+		options: { minify: true },
+	}));
+
+export default site;
