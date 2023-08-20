36573462b5c3349a349d0108aaa064d4c7c576cb
Filip Seman <filip.seman@protonmail.com>
2023-08-20 08:53:13 +0200

    feat: add meta tags generator

--- _config.ts
+++ _config.ts
@@ -5,6 +5,7 @@ import sitemap from "lume/plugins/sitemap.ts";
 import inline from "lume/plugins/inline.ts";
 import minifyHTML from "lume/plugins/minify_html.ts";
 import lightningCss from "lume/plugins/lightningcss.ts";
+import metas from "lume/plugins/metas.ts";
 
 const site = lume({
 	/** required for sitemap() */
@@ -15,6 +16,7 @@ site
 	.copy("css")
 	.copy("img")
 	.copy("svg")
+	.use(metas())
 	.use(sitemap())
 	.use(inline())
 	.use(minifyHTML())
